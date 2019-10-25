8cbf7f99eab8d5fc27e7ddad897817df9f480804
Cloudbeauty <1003382054@qq.com>
2019-10-25 19:45:02 +0800

    header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,20 +1,26 @@
 <template>
 	<div>
 		<detail-banner></detail-banner>
+		<detail-header></detail-header>
+		<div class="content"></div>
 	</div>	
 </template>
 
 <script>
 import detailBanner from './components/Banner'
+import detailHeader from './components/Header'
 
 export default {
 	name: 'detail',
 	components: {
 		detailBanner,
+		detailHeader
 	}
 }
 </script>
 
 <style lang="stylus" scoped>
-	
+.content{
+	height: 50rem;
+}	
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,89 @@
+<template>
+<div>
+	<router-link 
+		tag="div" to="/" 
+		class="header-abs" v-show="showAbs">
+		<div class="iconfont header-abs-back">&#xe624;</div>
+	</router-link>
+	<div class="header-fixed" v-show="!showAbs"
+		:style="opacityStyle">
+		<router-link to="/">
+			<div class="iconfont header-fixed-back">&#xe624;</div>
+		</router-link>
+		景点详情
+	</div>
+</div>	
+</template>
+
+<script>
+
+export default {	
+	name: 'detailHearder',
+	data () {
+		return {
+			showAbs: true,
+			opacityStyle: {
+				opacity: 0
+			}
+		}
+	},
+	methods: {
+		handleScroll () {
+			//document.documentElement.scrollTop = 0
+			const top = document.body.scrollTop;
+			//header渐隐渐现效果
+			if (top > 50) {
+				let opacity = top / 140;
+				opacity = opacity > 1? 1: opacity;
+				this.opacityStyle = {
+					opacity
+				}
+				this.showAbs = false;
+			}else{
+				this.showAbs = true;
+			}
+		}
+	},
+	//activated需要和keep-alive配合使用
+	created () {
+		window.addEventListener('scroll',this.handleScroll)
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+
+.header-abs
+	position: absolute;
+	left: .2rem;
+	top: .2rem;
+	width: .8rem;
+	height: .8rem;
+	line-height: .8rem;
+	border-radius: .4rem;
+	text-align: center;
+	background: rgba(0,0,0,0.6);
+	.header-abs-back
+		color: #fff;
+		font-size: .4rem;
+.header-fixed
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	height: $headerHeight;
+	line-height: $headerHeight;
+	color: #fff;
+	text-align: center;
+	background: $bgColor;
+	font-size: .32rem;
+	.header-fixed-back
+		width: .64rem;
+		text-align: center;
+		font-size: .4rem;
+		position: absolute;
+		top: 0;
+		left: 0;
+		color: #fff;
+</style>
